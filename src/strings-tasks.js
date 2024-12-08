0e74350d3be983e6ff14c21b143149f8266d012a
romanplis <plisroman@gmail.com>
2024-12-08 23:22:34 +0500

    feat: add new solution

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -489,8 +489,20 @@ function extractEmails(str) {
  *    => 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm'
  *
  */
-function encodeToRot13(/* str */) {
-  throw new Error('Not implemented');
+function encodeToRot13(str) {
+  let result = '';
+  for (let i = 0; i < str.length; i += 1) {
+    const n = str.charCodeAt(i);
+    if (n >= 65 && n <= 90) {
+      result += String.fromCharCode(((n - 65 + 13) % 26) + 65);
+    } else if (n >= 97 && n <= 122) {
+      result += String.fromCharCode(((n - 97 + 13) % 26) + 97);
+    } else {
+      result += str[i];
+    }
+  }
+  console.log(result);
+  return result;
 }
 
 /**
